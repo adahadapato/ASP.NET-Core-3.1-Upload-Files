1116b9d303e4f8e238a4d8265804cdd340982b9c
Adahada Enobong <adahadapato@gmail.com>
2021-12-31 10:31:29 +0100

    Client side code

--- IFilesRepository.cs
+++ IFilesRepository.cs
@@ -1,16 +1,13 @@
-﻿using demsworldapi.Dtos;
+﻿using demsworld.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace demsworldapi.Services
+namespace demsworld.Services
 {
     public interface IFilesRepository
     {
-        Task<(bool IsSuccess, IEnumerable<FileOutDTO> files, string Message)> GetAllFiles();
-        Task<(bool IsSuccess, IEnumerable<FileOutDTO> files, string Message)> GetFilesByType(string fileType);
-        Task<(bool IsSuccess, IEnumerable<FileOutDTO> files, string Message)> GetFilesByUploader(string email);
-        Task<(bool IsSuccess, FileOutDTO file, string Message)> GetFileById(int id);
-        Task<(bool IsSuccess, string Message)> SaveFile(FileInDTO model);
-        Task<(bool IsSuccess, string Message)> UpdateFile(FileInUpdateDTO model);
+        Task<(bool IsSuccess, IEnumerable<FilesViewModel> files, string Message)> GetAllFiles();
+        Task<(bool IsSuccess, FilesViewModel file, string Message)> GetFile(int Id);
+        Task<(bool IsSuccess, string Message)> Create(FilesViewModel model);
     }
 }
---
+++ IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using demsworld.Infrastructure;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace demsworld.Services
+{
+    public interface IGenericRepository
+    {
+        Task<HttpResponseMessage> GetAsync(string url, string token);
+        //Task<HttpResponseMessage> GetAsync(string url, string token);
+        //Task<HttpResponseMessage> GetAsync(string url, string token);
+        Task<HttpResponseMessage> PostAsync<T>(string url, T objToCreate, string token) where T: class;
+        Task<HttpResponseMessage> PostFormContentAsync<T>(string url, T objToCreate, string token) where T : class;
+        Task<HttpResponseMessage> UpdateAsync<T>(string url, T objToUpdate, string token) where T: class;
+        Task<HttpResponseMessage> DeleteAsync(string url, string token);
+        //Task<HttpResponseMessage> DeleteAsync(string url, string Id, string token);
+        Task<TResponse> CreateJsonResponse<TResponse>(HttpResponseMessage response)
+            where TResponse : ApiResponse, new();
+    }
+}
