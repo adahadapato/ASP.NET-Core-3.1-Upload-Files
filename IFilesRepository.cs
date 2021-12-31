dcca54688c48fdaefac09a74d2b2e257237e8291
Adahada Enobong <adahadapato@gmail.com>
2021-12-31 10:16:11 +0100

    Add files via upload
    This is the FilesRepository Interface

---
+++ IFilesRepository.cs
@@ -0,0 +1,16 @@
+﻿using demsworldapi.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace demsworldapi.Services
+{
+    public interface IFilesRepository
+    {
+        Task<(bool IsSuccess, IEnumerable<FileOutDTO> files, string Message)> GetAllFiles();
+        Task<(bool IsSuccess, IEnumerable<FileOutDTO> files, string Message)> GetFilesByType(string fileType);
+        Task<(bool IsSuccess, IEnumerable<FileOutDTO> files, string Message)> GetFilesByUploader(string email);
+        Task<(bool IsSuccess, FileOutDTO file, string Message)> GetFileById(int id);
+        Task<(bool IsSuccess, string Message)> SaveFile(FileInDTO model);
+        Task<(bool IsSuccess, string Message)> UpdateFile(FileInUpdateDTO model);
+    }
+}
